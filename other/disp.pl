1acf71b5d6087be2ac48513f8be8776f5a215f9b
Kyle Wolf <k_wolf@chillingdesigns.ca>
2025-05-19 16:04:06 -0400

    disp.pl
    swaps the unidentified display name and sprite with the identified version

---
+++ other/disp.pl
@@ -0,0 +1,54 @@
+# made by MaterialBlade
+# swaps the unidentified display name and sprite with the identified version
+# 
+# make sure you have itemInfo_EN.lua in the same folder as the perl script
+# do perl disp.pl to run in the command line to run it
+# don't ask me for help
+
+use strict;
+use warnings;
+#use Data::Dumper;
+
+package disp;
+
+main();
+
+sub main
+{	
+	my @temp;
+	my @output;
+	my $file = 'itemInfo_EN.lua';
+	my $i = 0;
+	
+	print "File can't be loaded\n" unless (-r $file);
+	return unless (-r $file);
+
+	{ open my $fp, '<', $file; @temp = <$fp> }
+
+	print "Converting...\n";
+
+	foreach my $line (@temp) {
+		push @output, $line;
+
+		if($line =~ /(\s+)(identifiedDisplayName = ")(.*)/)
+		{
+			$output[$i-3] = $1."un".$2."[?]".$3."\n";
+		}
+
+		if($line =~ /(\s+)(identifiedResourceName = ")(.*)/)
+		{
+			$output[$i-3] = $1."un".$2.$3."\n";
+		}
+
+		$i++;
+		#last if $i > 100;
+	}
+
+	open(FH, '>', "itemInfo_EN_new.lua") or die $!;
+	foreach my $line (@output) {
+		print FH $line;	
+	}
+	close(FH);
+
+	print "All done!\n";
+}
